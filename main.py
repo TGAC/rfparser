c410a212af0cae62aec3f3938e0531a2bda66854
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2023-07-06 11:42:07 +0100

    Add JournalName, BookTitle and SeriesTitle to XML output

--- main.py
+++ main.py
@@ -28,10 +28,17 @@ except ImportError:
 BASE_CR_URL = "https://api.crossref.org"
 BASE_RF_URL = "https://api.researchfish.com/restapi"
 
+KNOWN_BOOK_SERIES = {
+    "Advances in Experimental Medicine and Biology",
+    "Lecture Notes in Computer Science",
+    "Methods in Enzymology",
+    "Methods in Molecular Biology",
+}
+
 CR_TYPE_TO_XML_CATEGORY_ID = {
     "book-chapter": "2",
     "journal-article": "1",
-    "posted-content": "124",  # we may need to also check that CR subtype is "preprint"
+    "preprint": "124",
 }
 XML_CATEGORY_ID_TO_CATEGORY = {
     "1": "Journal Article",
@@ -102,6 +109,14 @@ def write_xml_output(pubs_with_doi: Dict[str, Dict[str, Any]], outfile: str) -> 
             ElementTree.SubElement(publication_el, "CategoryID").text = category_id
             ElementTree.SubElement(publication_el, "DOI").text = doi
             ElementTree.SubElement(publication_el, "Title").text = pub["title"]
+            if category_id in {"1", "124"}:
+                # a journal article or preprint
+                ElementTree.SubElement(publication_el, "JournalName").text = pub["container-title"]
+            else:
+                # category_id == "2", i.e. a book chapter
+                ElementTree.SubElement(publication_el, "BookTitle").text = pub["container-title"]
+                if "series-title" in pub:
+                    ElementTree.SubElement(publication_el, "SeriesTitle").text = pub["series-title"]
     xml_tree = ElementTree.ElementTree(root_el)
     indent(xml_tree, space="\t")
     xml_tree.write(outfile, encoding="utf-8", xml_declaration=True)
@@ -184,7 +199,54 @@ def main() -> None:
             title = " ".join(itertools.chain.from_iterable(title_part.split() for title_part in pub_metadata["title"]))
             title = strip_tags(title)
             pub["title"] = title
-            pub["type"] = pub_metadata["type"]
+
+            pub_type = pub_metadata["type"]
+            if pub_type == "posted-content":
+                assert (
+                    pub_metadata["subtype"] == "preprint"
+                ), f"DOI {doi} is of type '{pub_type}' with unknown subtype '{pub_metadata['pub_subtype']}'"
+                pub_type = pub_metadata["subtype"]
+            pub["type"] = pub_type
+            assert pub_type in CR_TYPE_TO_XML_CATEGORY_ID, f"DOI {doi} has unknown publication type {pub_type}"
+
+            container_title_list = pub_metadata["container-title"]
+            if len(container_title_list) == 0:
+                assert pub_type == "preprint", f"DOI {doi} of type {pub_type} has unexpected empty container-title"
+                institution = pub_metadata.get("institution")
+                if institution:
+                    assert (
+                        len(pub_metadata["institution"]) == 1
+                    ), f"DOI {doi} of type {pub_type} has institution with multiple or no elements: {pub_metadata['institution']}"
+                    container_title = pub_metadata["institution"][0]["name"]
+                elif "Research Square" in pub_metadata["publisher"]:
+                    container_title = "Research Square"
+                elif "PeerJ" in pub_metadata["publisher"]:
+                    container_title = pub_metadata["group-title"]
+                else:
+                    raise Exception(f"DOI {doi} of type {pub_type} has unknown journal")
+            elif len(container_title_list) == 1:
+                container_title = container_title_list[0]
+            else:
+                assert (
+                    pub_type == "book-chapter"
+                ), f"DOI {doi} of type {pub_type} has container-title with multiple elements: {container_title_list}"
+                assert (
+                    len(container_title_list) == 2
+                ), f"DOI {doi} of type {pub_type} has container-title with more than 2 elements: {container_title_list}"
+                # the book title and book series are not in a fixed order
+                if container_title_list[0] in KNOWN_BOOK_SERIES:
+                    container_title, pub["series-title"] = reversed(container_title_list)
+                else:
+                    if container_title_list[1] not in KNOWN_BOOK_SERIES:
+                        log.warning(
+                            "DOI %s of type %s has container-title with unknown book series: %s",
+                            doi,
+                            pub_type,
+                            container_title_list,
+                        )
+                    container_title, pub["series-title"] = container_title_list
+            pub["container-title"] = container_title
+
             pub["metadata_ok"] = True
         except Exception as e:
             log.error("Skipping publication '%s': %s", doi, e)
